3f8d9ef9e8f3f8cfdbd838d957481108eefed41a
Daniel Zhang <zhang.ling1@husky.neu.edu>
2017-03-23 00:25:25 -0400

    try fix bug
    try fix bug, modify config

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -1,14 +1,14 @@
 module.exports = function () {
-    var connectionString = 'mongodb://127.0.0.1:27017/test'
-    if (process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
-    }
+    // var connectionString = 'mongodb://127.0.0.1:27017/test'
+    // if (process.env.MLAB_USERNAME) {
+    //     connectionString = process.env.MLAB_USERNAME + ":" +
+    //         process.env.MLAB_PASSWORD + "@" +
+    //         process.env.MLAB_HOST + ':' +
+    //         process.env.MLAB_PORT + '/' +
+    //         process.env.MLAB_APP_NAME;
+    // }
     var mongoose = require("mongoose");  // npm install mongoose --save
-    mongoose.createConnection(connectionString);
+    // mongoose.createConnection(connectionString);
     var model = {
         userModel: require("./user/user.model.server.js")(),
         websiteModel: require("./website/website.model.server.js")(),
